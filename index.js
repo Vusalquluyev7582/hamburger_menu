985b756963d5340bd308687b70e98e7245268eea
Vüsal Quliyev <vquluyev274@gmail.com>
2023-10-27 21:07:22 +0400

    proyect

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let dropdown = document.querySelector(".dropdown")
+let dr = document.querySelector(".dr")
+
+dropdown.style.display = "none"
+
+function drad() {
+    dropdown.style.display = "block"
+    dr.style.display = "none"
+}
+function myFunction() {
+    dropdown.style.display = "none"
+    dr.style.display = "block"
+}
